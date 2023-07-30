0e8ae4277ca1f0d597ba0b6ec2845f46631b0e34
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-07-30 09:40:47 +0000

    bookers2-Kadai3

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def index
+    @user = current_user
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :introduction, :profile_image)
+    end
+
+end
